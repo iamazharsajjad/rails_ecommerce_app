e64604b9b546aaa2fb17ddcf371588e0aee5ccba
Azhar Sajjad <iamazharsajjad@gmail.com>
2023-07-25 02:16:40 +0500

    Rubocop error Resolved!

--- Gemfile
+++ Gemfile
@@ -59,8 +59,8 @@ gem 'font-awesome-rails'
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
-  gem 'rspec-rails', '~> 6.0.0'
   gem 'rails-controller-testing'
+  gem 'rspec-rails', '~> 6.0.0'
 end
 
 group :development do
--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Products', type: :request do
-  describe "GET index" do
-    it "assigns @products" do
+  describe 'GET index' do
+    it 'assigns @products' do
       product = Product.create({ title: 'Product 1', description: 'Product 1 Description', price: 10, currency: '$' })
       get products_path
       expect(assigns(:products)).to eq([product])
@@ -13,8 +13,8 @@ RSpec.describe 'Products', type: :request do
     end
   end
 
-  describe "GET show" do
-    it "assigns @products" do
+  describe 'GET show' do
+    it 'assigns @products' do
       product = Product.create({ title: 'Product 1', description: 'Product 1 Description', price: 10, currency: '$' })
       get product_path(product.id)
       # expect(assigns(:products)).to eq([product])
@@ -22,5 +22,4 @@ RSpec.describe 'Products', type: :request do
       # expect(response.body).to include(brand.name)
     end
   end
-
 end
