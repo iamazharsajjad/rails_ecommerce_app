a5439c7b642ed7dc243b039fa4ad620ecbee0651
Azhar Sajjad <iamazharsajjad@gmail.com>
2023-07-25 02:12:38 +0500

    Product Controller Error!

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,5 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # describe 'columns' do
+  #   it { is_expected.to have_db_column(:title).of_type(:string) }
+  #   it { is_expected.to have_db_column(:description).of_type(:string) }
+  #   it { is_expected.to have_db_column(:stripe_id).of_type(:string) }
+  #   it { is_expected.to have_db_column(:currency).of_type(:string) }
+  #   it { is_expected.to have_db_column(:price).of_type(:integer) }
+  #   it { is_expected.to have_db_column(:sale_price).of_type(:integer) }
+  # end
 end
--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -3,7 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe 'Products', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET index" do
+    it "assigns @products" do
+      product = Product.create({ title: 'Product 1', description: 'Product 1 Description', price: 10, currency: '$' })
+      get products_path
+      expect(assigns(:products)).to eq([product])
+      expect(response).to have_http_status(200)
+      # expect(response.body).to include(brand.name)
+    end
   end
+
+  describe "GET show" do
+    it "assigns @products" do
+      product = Product.create({ title: 'Product 1', description: 'Product 1 Description', price: 10, currency: '$' })
+      get product_path(product.id)
+      # expect(assigns(:products)).to eq([product])
+      expect(response).to have_http_status(200)
+      # expect(response.body).to include(brand.name)
+    end
+  end
+
 end
