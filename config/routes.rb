a84f0ddd0669690e09bc2ea7f7276ff2807b9d53
Azhar Sajjad <iamazharsajjad@gmail.com>
2023-07-25 14:34:05 +0500

    Profile controller created!

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,13 @@ Rails.application.routes.draw do
   devise_for :customers, controllers: {
     registrations: 'customers/registrations'
   }
+  root 'home#index'
+  # resource :profile, :only => [:edit, :update]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root 'home#index'
   resources :products
 end
