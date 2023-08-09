29e51d4191706870d00f5fb439e5c123f97eda4b
iamazharsajjad <52309475+iamazharsajjad@users.noreply.github.com>
2023-08-09 18:27:13 +0500

    Revert "WhiteLableIps Added!"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_08_132033) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_06_143531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -115,13 +115,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_08_132033) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_profiles_on_customer_id"
-  end
-
-  create_table "white_listed_ips", force: :cascade do |t|
-    t.string "source_ip"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
